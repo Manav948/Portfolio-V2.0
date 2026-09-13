3f19eb9df229bc5842ac7acfed39d195764d59d3
Manav948 <valanimanav61@gmail.com>
2026-09-13 13:44:16 +0530

    added leetcode button and its profile

--- components/Header.tsx
+++ components/Header.tsx
@@ -51,7 +51,7 @@ const Header: React.FC<HeaderProps> = ({
         </div>
 
        
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-3 sm:gap-4">
           <GithubButton href={githubUrl} />
           <TwitterButton href={twitterUrl} />
         </div>
